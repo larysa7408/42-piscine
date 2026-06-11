7dd9a7fec4d531da8d79ec2cf3a748648c46e3e4
larysa7408 <larisa7408tema@gmail.com>
2026-06-11 22:31:51 +0200

    add main

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -3,10 +3,10 @@
 /*                                                       :::      ::::::::    */
 /*   ft_div_mod.c                                      :+:      :+:    :+:    */
 /*                                                   +:+ +:+         +:+      */
-/*   By: lsatthof <lsatthof@student.42wolfsburg.de>#+#  +:+       +#+         */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/05/13 17:32:31 by lsatthof         #+#    #+#              */
-/*   Updated: 2026/05/13 17:32:31 by lsatthof        ###   ########.fr        */
+/*   Created: 2026/05/13 17:32:31 by username         #+#    #+#              */
+/*   Updated: 2026/06/11 22:31:19 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,16 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*div = a / b;
 	*mod = a % b;
 }
+
+// #include<stdio.h>
+
+// int	main(void)
+// {
+// 	int	result_div;
+// 	int	result_mod;
+
+// 	ft_div_mod(42, 10, &result_div, &result_mod);
+// 	printf("dvision = %d\n", result_div);
+// 	printf("remaider = %d\n", result_mod);
+// 	return (0);
+// }
