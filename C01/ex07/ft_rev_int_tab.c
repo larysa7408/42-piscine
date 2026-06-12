285c34b5010439b1ae83e16b3d399c723b1aa580
larysa7408 <larisa7408tema@gmail.com>
2026-06-13 00:09:13 +0200

    add main

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -3,10 +3,10 @@
 /*                                                       :::      ::::::::    */
 /*   ft_rev_int_tab.c                                  :+:      :+:    :+:    */
 /*                                                   +:+ +:+         +:+      */
-/*   By: lsatthof <lsatthof@student.42wolfsburg.de>#+#  +:+       +#+         */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/05/13 17:33:00 by lsatthof         #+#    #+#              */
-/*   Updated: 2026/05/13 17:33:00 by lsatthof        ###   ########.fr        */
+/*   Created: 2026/05/13 17:33:00 by username         #+#    #+#              */
+/*   Updated: 2026/06/13 00:08:24 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,26 @@ void	ft_rev_int_tab(int *tab, int size)
 		end--;
 	}
 }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	int	tab[5];
+// 	int	i;
+
+// 	tab[0] = 1;
+// 	tab[1] = 2;
+// 	tab[2] = 3;
+// 	tab[3] = 4;
+// 	tab[4] = 5;
+// 	ft_rev_int_tab(tab, 5);
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		printf("%d ", tab[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	return (0);
+// }
