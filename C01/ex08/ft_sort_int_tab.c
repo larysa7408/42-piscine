27021af387568cdca020ecaa6e08252bfcbd5e68
larysa7408 <larisa7408tema@gmail.com>
2026-06-13 12:55:28 +0200

    add main

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -3,10 +3,10 @@
 /*                                                       :::      ::::::::    */
 /*   ft_sort_int_tab.c                                 :+:      :+:    :+:    */
 /*                                                   +:+ +:+         +:+      */
-/*   By: lsatthof <lsatthof@student.42wolfsburg.de>#+#  +:+       +#+         */
+/*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
-/*   Created: 2026/05/13 17:33:07 by lsatthof         #+#    #+#              */
-/*   Updated: 2026/05/13 17:33:15 by lsatthof        ###   ########.fr        */
+/*   Created: 2026/05/13 17:33:07 by username         #+#    #+#              */
+/*   Updated: 2026/06/13 12:54:57 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,43 @@ void	ft_sort_int_tab(int *tab, int size)
 	int	j;
 	int	temp;
 
-	j = 0;
-	while (j < size - 1)
+	i = 0;
+	while (i < size - 1)
 	{
-		i = 0;
-		while (i < size - 1)
+		j = 0;
+		while (j < size - 1 - i)
 		{
-			if (tab[i] > tab[i + 1])
+			if (tab[j] > tab[j + 1])
 			{
-				temp = tab[i];
-				tab[i] = tab[i + 1];
-				tab[i + 1] = temp;
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
 			}
-			i++;
+			j++;
 		}
-		j++;
+		i++;
 	}
 }
+
+// #include<stdio.h>
+
+// int	main(void)
+// {
+// 	int	tab[5];
+// 	int	i;
+
+// 	tab[0] = 5;
+// 	tab[1] = 2;
+// 	tab[2] = 8;
+// 	tab[3] = 1;
+// 	tab[4] = 4;
+// 	ft_sort_int_tab(tab, 5);
+// 	i = 0;
+// 	while (i < 5)
+// 	{
+// 		printf("%d ", tab[i]);
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	return (0);
+// }
